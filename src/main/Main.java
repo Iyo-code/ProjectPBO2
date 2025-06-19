01f0b5468e9e2d9c8cc9ea4ad8b2abc06ca09a6d
IyooAnjayMabar <anjaytrio913@gmail.com>
2025-06-19 22:52:45 +0800

    perbaiki susunan package yang salah dan main berisi end point villa, customer, voucher

--- src/main/Main.java
+++ src/main/Main.java
@@ -31,6 +31,14 @@ public class Main {
             CustomerService.handle(exchange);
         });
 
+        // Voucher
+        server.createContext("/vouchers", exchange -> {
+            if (!ApiKeyFilter.check(exchange, API_KEY))
+                return;
+
+            VoucherService.handle(exchange);
+        });
+
         server.setExecutor(null); // default
         server.start();
 
